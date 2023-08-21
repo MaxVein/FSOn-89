c6b16caf7c52fcc7990b29cd3f434051acead1cb
MaxVein <makslav67@gmail.com>
2023-08-21 19:16:54 +0300

    module 8 practice-1

---
+++ module-8/practice-1/src/index.js
@@ -0,0 +1,39 @@
+// *****Встановлення збірки parcel-project-template*****
+// https://github.com/goitacademy/parcel-project-template
+
+// *****Перепідключення репозиторію*****
+// git remote -v
+
+// git remote set-url origin посилання на свій репозиторій
+
+// git remote -v
+
+// *****Налаштування конфігурацій файлу package.json*****
+
+// *****Інсталювання залежностей збірки*****
+// npm install
+
+// *****Запуск збірки*****
+// npm start
+
+// *****Налаштування GitHub*****
+// Надати read & write доступи
+
+// *****Пуш на GitHub*****
+
+// *****Налаштування GitHub Pages*****
+
+// *****Навести приклади import/export*****
+
+// Функції
+function add(a, b) {
+  console.log('value', value);
+  return a + b;
+}
+
+// Класи
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
